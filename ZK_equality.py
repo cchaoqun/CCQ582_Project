d3e4c0f86901c624a710e3c76a3f588f78097d27
程超群 <chengchaoqun@bytedance.com>
2022-04-03 17:19:46 +0800

    finish project3.2

---
+++ ZK_equality.py
@@ -0,0 +1,23 @@
+from zksk import Secret, DLRep
+from zksk import utils
+
+def ZK_equality(G,H):
+
+    #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
+    # Setup: generate a secret randomizer for the commitment scheme.
+    r1 = Secret(utils.get_random_num(bits=128))
+    r2 = Secret(utils.get_random_num(bits=128))
+    m = Secret(utils.get_random_num(bits=128))
+    #Generate a NIZK proving equality of the plaintexts
+    '''
+    C1C2D1D2=r1∗G=r1∗H+m∗G=r2∗G=r2∗H+m∗G
+    '''
+    C1 = r1 * G
+    C2 = r1 * H + m * G
+    D1 = r2 * G
+    D2 = r2 * H + m * G
+    stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
+    zk_proof = stmt.prove()
+    #Return two ciphertexts and the proof
+    return (C1,C2), (D1,D2), zk_proof
+
